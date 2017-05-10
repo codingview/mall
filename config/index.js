3433ebe783553b066bd337a410a7b401130929dc
zrz <occultskyrong@163.com>
2017-05-10 17:05:16 +0800

    【新增】项目结构
    1、【更新】授权协议。
    2、【新增】配置信息。
    3、【新增】工具类。
    4、【新增】基础ejs模板。
    5、【新增】其他项目结构内容。

---
+++ config/index.js
@@ -0,0 +1,65 @@
+/**
+ * Created by zrz on 2017/4/27.
+ * Copyright© 2015-2020 occultskyrong (https://github.com/diandainfo)
+ * @version 0.0.1 created
+ */
+
+"use strict";
+
+const _ = {
+    development: {
+        port: 80
+        , host: ''
+        , url: {
+            api_server: ''
+        }, redis: {
+            host: '192.168.1.101'
+            , port: 6379
+            , db: 6
+            , ttl: 86000
+            , secret: "business_statistics_web"
+            , key: "business_statistics_web"
+        }
+    }, test: {
+        port: 30218
+        , host: ''
+        , url: {
+            api_server: ''
+        }, redis: {
+            host: '192.168.1.101'
+            , port: 6379
+            , db: 6
+            , ttl: 86000
+            , secret: "business_statistics_web"
+            , key: "business_statistics_web"
+        }
+    }, experience: {
+        port: 30218
+        , host: ''
+        , url: {
+            api_server: ''
+        }, redis: {
+            host: "127.0.0.1"
+            , port: 6379
+            , db: 6
+            , ttl: 86000
+            , secret: "business_statistics_web"
+            , key: "business_statistics_web"
+        }
+    }, production: {
+        port: 30218
+        , host: ''
+        , url: {
+            api_server: ''
+        }, redis: {
+            host: "192.168.0.251"
+            , port: 6379
+            , db: 6
+            , ttl: 86000
+            , secret: "business_statistics_web"
+            , key: "business_statistics_web"
+        }
+    }
+};
+
+module.exports = _[BSW.ENV];
---
+++ config/log4js.js
@@ -0,0 +1,107 @@
+/**
+ * Created by zrz on 2017/4/27.
+ * Copyright© 2015-2020 DianDaInfo (https://github.com/diandainfo)
+ * @version 0.0.1 created
+ */
+
+"use strict";
+
+module.exports = {
+    development: {
+        appenders: [{
+            type: "console"
+        }, {
+            type: "clustered"
+            , appenders: [{
+                "type": "dateFile",
+                "filename": "./logs/http/http.log",//请求日志
+                "pattern": "-yyyy-MM-dd",
+                "category": "http"
+            }, {
+                "type": "file",
+                "filename": "./logs/app.log",//运行日志
+                "maxLogSize": 10485760,
+                "numBackups": 3
+            }, {
+                "type": "logLevelFilter",
+                "level": "ERROR",
+                "appender": {
+                    "type": "file",
+                    "filename": "./logs/errors.log"//错误日志
+                }
+            }]
+        }]
+    },test: {
+        appenders: [{
+            type: "console"
+        }, {
+            type: "clustered"
+            , appenders: [{
+                "type": "dateFile",
+                "filename": "./logs/http/http.log",//请求日志
+                "pattern": "-yyyy-MM-dd",
+                "category": "http"
+            }, {
+                "type": "file",
+                "filename": "./logs/app.log",//运行日志
+                "maxLogSize": 10485760,
+                "numBackups": 3
+            }, {
+                "type": "logLevelFilter",
+                "level": "ERROR",
+                "appender": {
+                    "type": "file",
+                    "filename": "./logs/errors.log"//错误日志
+                }
+            }]
+        }]
+    },experience: {
+        appenders: [{
+            type: "console"
+        }, {
+            type: "clustered"
+            , appenders: [{
+                "type": "dateFile",
+                "filename": "./logs/http/http.log",//请求日志
+                "pattern": "-yyyy-MM-dd",
+                "category": "http"
+            }, {
+                "type": "file",
+                "filename": "./logs/app.log",//运行日志
+                "maxLogSize": 10485760,
+                "numBackups": 3
+            }, {
+                "type": "logLevelFilter",
+                "level": "ERROR",
+                "appender": {
+                    "type": "file",
+                    "filename": "./logs/errors.log"//错误日志
+                }
+            }]
+        }]
+    },production: {
+        appenders: [{
+            type: "console"
+        }, {
+            type: "clustered"
+            , appenders: [{
+                "type": "dateFile",
+                "filename": "./logs/http/http.log",//请求日志
+                "pattern": "-yyyy-MM-dd",
+                "category": "http"
+            }, {
+                "type": "file",
+                "filename": "./logs/app.log",//运行日志
+                "maxLogSize": 10485760,
+                "numBackups": 3
+            }, {
+                "type": "logLevelFilter",
+                "level": "ERROR",
+                "appender": {
+                    "type": "file",
+                    "filename": "./logs/errors.log"//错误日志
+                }
+            }]
+        }]
+    }
+};
